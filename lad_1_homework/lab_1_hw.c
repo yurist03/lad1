27066d27cb4c65afde040a4c3f3c5594e8ec5a20
yurist03 <yura.kapichnikov.07@bk.ru>
2025-09-15 20:49:39 +0300

    Add files via upload

---
+++ lad_1_homework/lab_1_hw.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    puts("    /\\ ");
+    puts("   /  \\ ");
+    puts("  /    \\ ");
+    puts(" /______\\ ");
+    puts(" |      |");
+    puts(" |  []  |");
+    puts(" |      |");
+    puts(" |______|");
+    return 0;
+}
